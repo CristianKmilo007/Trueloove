68238b34caef933fb91c135a8539d7f76761eb9c
JoseBELT <robert.bvenegas1@hotmail.com>
2022-08-10 20:33:11 -0500

    form settings

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
 import { PleasuresComponent } from './views/pleasures/pleasures.component';
+import { SettingsComponent } from './views/settings/settings.component';
 import { SignupComponent } from './views/signup/signup.component';
 
 const routes: Routes = [
   {path: "home", component: HomeComponent},
   {path: "signup", component: SignupComponent},
   {path: "pleasures", component: PleasuresComponent},
+  {path: "settings", component: SettingsComponent},
 
   {path: "", redirectTo: "/home", pathMatch: "full"},
   {path: "**", component: HomeComponent}
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './views/home/home.component';
 import { SignupComponent } from './views/signup/signup.component';
 import { PleasuresComponent } from './views/pleasures/pleasures.component';
+import { SettingsComponent } from './views/settings/settings.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { PleasuresComponent } from './views/pleasures/pleasures.component';
     LoginComponent,
     HomeComponent,
     SignupComponent,
-    PleasuresComponent
+    PleasuresComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule,
